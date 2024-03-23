b7eb09fb8a6289ff94d47363ff710b61d2127950
shafaqanees123 <148550198+shafaqanees123@users.noreply.github.com>
2024-03-23 11:40:10 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([{
+    name: " userGuessNumber",
+    type: "number",
+    message: "please guess a number 1-6:",
+},]);
+if (answers.userGuessNumber === randomNumber){
+    console.log("Congratulation! you guessed right number."
+    );
+} else {
+    console.log("you guessed wrong number");}
